ce281a43a992de1f2831a14905b497bb9b0bcc06
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-01-08 15:22:37 +0100

    Debut du menu et upload des premiers assets

--- display.py
+++ display.py
@@ -7,6 +7,14 @@ class Display:
         pygame.init()
         self.screen = pygame.display.set_mode((self.__WIDTH, self.__HEIGHT))
         pygame.display.set_caption('Pokemon')
+        
+        # Menu assets (passer par une fonction?)
+        self.menu_background = pygame.image.load("assets/images/menu/background.png")
+        self.menu_background = pygame.transform.scale(self.menu_background, (self.__WIDTH, self.__HEIGHT))
+        self.menu_title = pygame.image.load("assets/images/menu/title.png")
+        self.menu_title = pygame.transform.scale(self.menu_title,(self.menu_title.get_width() / 2, self.menu_title.get_height() / 2))
+
+    
     
     def draw_intro(self):
         pass
@@ -18,6 +26,9 @@ class Display:
     def draw_menu(self):
         '''Draws the menu elements'''
         self.screen.fill('purple')
+        self.screen.blit(self.menu_background,(0,0))
+        self.screen.blit(self.menu_title, (self.__WIDTH / 2 - self.menu_title.get_width() / 2,50))
+        
         
         
         pygame.display.update()
