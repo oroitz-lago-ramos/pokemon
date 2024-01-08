1c92d96c4ae83c68074aea99df29c22a88f54a60
thomas-chardin <thomas.chardin@laplateforme.io>
2024-01-08 15:36:40 +0100

    mise a jour du calcule de degats avec le systeme de defence

--- fight.py
+++ fight.py
@@ -1,3 +1,5 @@
+import json
+
 class Fight:
     def __init__(self):
         self.attacker_attack = 0
@@ -12,11 +14,14 @@ class Fight:
             'grass': {'water': 2, 'fire': 0.5, 'grass': 1, 'neutral': 1},
             'neutral': {'water': 0.75, 'fire': 0.75, 'grass': 0.75, 'neutral': 1}
         }
-    # voir comment mettre le lien avec les fichiers json pour le calcule
-        
-    def apply_defense(self):
-        pass
-    # voir comment la defense influ sur les degats subit
+
+        damage_multiplier = type_chart.get(attacker_type, {}).get(defender_type, 1)
+        damage = attacker_attack * damage_multiplier
+        return damage
+
+    def apply_defense(self, current_hp, defense_points, attacker_attack):
+        adjusted_damage = attacker_attack / max(1, defense_points)
+        return adjusted_damage
 
     def determine_winner(self, attacker_hp, defender_hp):
         if attacker_hp > defender_hp:
@@ -25,7 +30,7 @@ class Fight:
             return "Defender"
         else:
             return "Draw"
-        
+
     def battle_result(self, attacker_name, defender_name, attacker_hp, defender_hp):
         winner = self.determine_winner(attacker_hp, defender_hp)
         if winner == "Attacker":
