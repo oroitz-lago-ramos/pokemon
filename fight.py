da6345bea5e38785960020fc02e91bd845339be2
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-01-09 13:51:46 +0100

    Reset de la classe fight

--- fight.py
+++ fight.py
@@ -7,11 +7,6 @@ class Fight:
         self.data_manager = Data_manager()
 
     def start_fight(self, player_pokemon_name, enemy_pokemon_name):
-        player_pokemon_data = self.data_manager.get_pokemon_data(player_pokemon_name)
-        player_pokemon_attacks_data = self.data_manager.get_attack_data(player_pokemon_name)
-        enemy_pokemon_data = self.data_manager.get_pokemon_data(enemy_pokemon_name)
-        enemy_pokemon_attacks_data = self.data_manager.get_attack_data(player_pokemon_name)
-    
-        player_pokemon = Pokemon(player_pokemon_data)
-        enemy_pokemon = Pokemon(enemy_pokemon_data)
+        player_pokemon = Pokemon(player_pokemon_name)
+        enemy_pokemon = Pokemon(enemy_pokemon_name)
 
