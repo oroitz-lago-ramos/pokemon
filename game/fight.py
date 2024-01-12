c0b5fe04e943adf13883775131958f5e50f4631d
fares-fraoucene <fares.fraoucene@laplateforme.io>
2024-01-12 13:20:39 +0100

    def getter player pokemon

--- game/fight.py
+++ game/fight.py
@@ -9,3 +9,6 @@ class Fight:
     def start_fight(self, player_pokemon_name, enemy_pokemon_name):
         self.player_pokemon = game.Pokemon(player_pokemon_name, self.game.data_manager)
         self.enemy_pokemon = game.Pokemon(enemy_pokemon_name, self.game.data_manager)
+    def get_player_pokemon(self):
+        return self.player_pokemon
+    
