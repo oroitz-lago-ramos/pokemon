ed058c1065b7ada691d14754b5b1a1b01006fff8
OroitzLR <oroitzlr@gmail.com>
2024-01-14 23:39:06 +0100

    Ajout f combat

--- game/fight.py
+++ game/fight.py
@@ -1,4 +1,6 @@
 import game
+import time
+
 class Fight:
     def __init__(self, game) -> None:
         self.game = game
@@ -9,3 +11,18 @@ class Fight:
     def start_fight(self, player_pokemon_name, enemy_pokemon_name):
         self.player_pokemon = game.Pokemon(player_pokemon_name)
         self.enemy_pokemon = game.Pokemon(enemy_pokemon_name)
+        self.run()
+
+    def run(self):
+        while self.player_pokemon.isAlive and self.enemy_pokemon.isAlive:
+            self.player_turn()
+            
+            time.sleep(1)
+
+            self.enemy_turn()
+
+    def player_turn(self):
+        pass
+
+    def enemy_turn(self):
+        pass
