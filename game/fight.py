0740db6cec0fcbdb1bb8a270ab48706bfc9f8817
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-01-16 20:28:45 +0100

    comment une fonction qui ne marche pas encore

--- game/fight.py
+++ game/fight.py
@@ -11,7 +11,7 @@ class Fight:
     def start_fight(self, player_pokemon_name, enemy_pokemon_name):
         self.player_pokemon = game.Pokemon(player_pokemon_name)
         self.enemy_pokemon = game.Pokemon(enemy_pokemon_name)
-        self.run_end()
+        # self.run_end()
 
     def run_end(self):
         while self.player_pokemon.isAlive and self.enemy_pokemon.isAlive:
