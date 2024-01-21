c9be616c13d54710d7141a3a0bfd9d0e1050ac76
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-01-21 12:18:46 +0100

    Aucun changement particulir juste pour pouvoir switch de branche et merge

--- graphics/combat.py
+++ graphics/combat.py
@@ -19,7 +19,6 @@ class Combat:
         self.player_life_bar = pygame.transform.scale2x(self.player_life_bar)
         self.enemy_life_bar = pygame.transform.scale2x(self.enemy_life_bar)
         
-        
         #Trouver les memes tailles de sprites afin de enlever le pygame transform et ces variables là
         
         self.pokemon_bounce = 0
@@ -88,11 +87,11 @@ class Combat:
         self.text.draw_text(self.fight.enemy_pokemon.get_name(), 12, (82, 63))
         self.text.draw_text(str(self.fight.enemy_pokemon.get_level()), 12, (224, 64))
         self.text.draw_text(str(self.fight.player_pokemon.get_level()), 12, (722, 390))
-        pygame.draw.rect(self.display.screen, self.hp_bar_color(self.fight.enemy_pokemon), (137, 82, self.fight.enemy_pokemon.get_health() / self.fight.enemy_pokemon.get_max_health() * 100 - 1, 9))
-        # self.fight.enemy_pokemon.take_damage(0.1)
-        pygame.draw.rect(self.display.screen, self.hp_bar_color(self.fight.player_pokemon), (635, 408, self.fight.player_pokemon.get_health() / self.fight.player_pokemon.get_max_health() * 100 - 1, 9))
-        self.fight.player_pokemon.take_damage(0.1)
-    
+        pygame.draw.rect(self.display.screen, self.hp_bar_color(self.fight.enemy_pokemon), (139, 84, self.fight.enemy_pokemon.get_health() / self.fight.enemy_pokemon.get_max_health() * 100 - 5, 6))
+        pygame.draw.rect(self.display.screen, self.hp_bar_color(self.fight.player_pokemon), (636, 410, self.fight.player_pokemon.get_health() / self.fight.player_pokemon.get_max_health() * 100 - 4, 6))
+        self.text.draw_text("Attaquer",20, (150, 515))
+        self.text.draw_text("Fuir",20, (520, 515))
+        
     def get_sprite(self, sheet, x, y, width, height, size):
         sprite = pygame.Surface((width, height))
         sprite.blit(sheet, (0, 0), (x, y, width, height))
